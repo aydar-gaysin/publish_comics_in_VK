60c46cfe1e46aa939a324b4b90cc529f67eb55b7
aydar.gaysin@gmail.com <4r8hMBYjPh5Gmyk>
2021-05-07 11:08:52 +0300

    Refactored to write image file right to directory with script

--- main.py
+++ main.py
@@ -5,32 +5,29 @@ import requests
 from dotenv import load_dotenv
 
 
-DIR_PATH = 'comics'
 XKCD_URL = 'https://xkcd.com/'
 POSTFIX_URL = '/info.0.json'
 VK_API_URL = 'https://api.vk.com/method/'
 
 
-def generate_random_comic_id():
+def generate_random_comic_id(xkcd_url, postfix_url):
     full_url = f'{xkcd_url}{postfix_url}'
     response = requests.get(full_url)
     response.raise_for_status()
     api_response = response.json()
     last_image_id = api_response['num']
-    random_id = random.randint(1,last_image_id)
+    random_id = random.randint(1, last_image_id)
     return random_id
 
 
-def fetch_xkcd_comic(directory, xkcd_url, postfix_url):
-    comic_id = '353'
-    full_url = f'{xkcd_url}{comic_id}{postfix_url}'
+def fetch_xkcd_comic(random_comic_id, xkcd_url, postfix_url):
+    full_url = f'{xkcd_url}{random_comic_id}{postfix_url}'
     response = requests.get(full_url)
     response.raise_for_status()
     api_response = response.json()
     image_url = api_response
 
-    with open(os.path.join(directory, f'xkcd_{comic_id}.png'), 'wb') as \
-            file:
+    with open(f'xkcd_{random_comic_id}.png', 'wb') as file:
         get_image = requests.get(image_url['img'])
         message = image_url['alt']
         get_image.raise_for_status()
@@ -51,8 +48,8 @@ def request_upload_url(access_token, vk_group_id, vk_api_url):
     return get_upload_url
 
 
-def upload_photo_to_server(dir_path, upload_url):
-    with open(f'{dir_path}/xkcd_353.png', 'rb') as file:
+def upload_photo_to_server(random_comic_id, upload_url):
+    with open(f'xkcd_{random_comic_id}.png', 'rb') as file:
         files = {
             'photo': file,
         }
@@ -105,9 +102,10 @@ def main():
     load_dotenv()
     access_token = os.getenv('ACCESS_TOKEN')
     vk_group_id = os.getenv('GROUP_ID')
-    os.makedirs(DIR_PATH, exist_ok=True)
+    random_comic_id = generate_random_comic_id(XKCD_URL, POSTFIX_URL)
+    print(random_comic_id)
     try:
-        title = fetch_xkcd_comic(DIR_PATH, XKCD_URL, POSTFIX_URL)
+        title = fetch_xkcd_comic(random_comic_id, XKCD_URL, POSTFIX_URL)
         print(title)
     except requests.exceptions.HTTPError as error:
         exit('Ошибка:\n{0}'.format(error))
@@ -115,7 +113,7 @@ def main():
     if title:
         upload_url = request_upload_url(access_token, vk_group_id, VK_API_URL)
 
-        upload_comics = upload_photo_to_server(DIR_PATH, upload_url)
+        upload_comics = upload_photo_to_server(random_comic_id, upload_url)
         image_server = upload_comics['server']
         image_hash = upload_comics['hash']
         image_photo = upload_comics['photo']
