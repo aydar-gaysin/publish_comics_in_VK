2a12bc66648a928fd961110a2b3e8c72042cecae
aydar.gaysin@gmail.com <4r8hMBYjPh5Gmyk>
2021-05-07 12:04:55 +0300

    Added cleanup image file function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ POSTFIX_URL = '/info.0.json'
 VK_API_URL = 'https://api.vk.com/method/'
 
 
-def generate_random_comic_id(xkcd_url, postfix_url):
+def generate_random_comics_id(xkcd_url, postfix_url):
     full_url = f'{xkcd_url}{postfix_url}'
     response = requests.get(full_url)
     response.raise_for_status()
@@ -20,14 +20,14 @@ def generate_random_comic_id(xkcd_url, postfix_url):
     return random_id
 
 
-def fetch_xkcd_comic(random_comic_id, xkcd_url, postfix_url):
-    full_url = f'{xkcd_url}{random_comic_id}{postfix_url}'
+def fetch_xkcd_comics(random_comics_id, xkcd_url, postfix_url):
+    full_url = f'{xkcd_url}{random_comics_id}{postfix_url}'
     response = requests.get(full_url)
     response.raise_for_status()
     api_response = response.json()
     image_url = api_response
 
-    with open(f'xkcd_{random_comic_id}.png', 'wb') as file:
+    with open(f'xkcd_{random_comics_id}.png', 'wb') as file:
         get_image = requests.get(image_url['img'])
         message = image_url['alt']
         get_image.raise_for_status()
@@ -48,8 +48,8 @@ def request_upload_url(access_token, vk_group_id, vk_api_url):
     return get_upload_url
 
 
-def upload_photo_to_server(random_comic_id, upload_url):
-    with open(f'xkcd_{random_comic_id}.png', 'rb') as file:
+def upload_photo_to_server(random_comics_id, upload_url):
+    with open(f'xkcd_{random_comics_id}.png', 'rb') as file:
         files = {
             'photo': file,
         }
@@ -78,9 +78,8 @@ def save_uploaded_photo(
     return api_response['response'][0]
 
 
-def post_comic(
-        access_token, vk_group_id, vk_api_url, title, media_id,
-        media_owner_id
+def post_comics(
+        access_token, vk_group_id, vk_api_url, title, media_id, media_owner_id
 ):
     parameters = {
         'access_token': access_token,
@@ -98,19 +97,23 @@ def post_comic(
     return api_response
 
 
+def delete_comics_file(random_comics_id):
+    os.remove(f'xkcd_{random_comics_id}.png')
+
+
 def main():
     load_dotenv()
     access_token = os.getenv('ACCESS_TOKEN')
     vk_group_id = os.getenv('GROUP_ID')
-    random_comic_id = generate_random_comic_id(XKCD_URL, POSTFIX_URL)
+    random_comics_id = generate_random_comics_id(XKCD_URL, POSTFIX_URL)
     try:
-        title = fetch_xkcd_comic(random_comic_id, XKCD_URL, POSTFIX_URL)
+        title = fetch_xkcd_comics(random_comics_id, XKCD_URL, POSTFIX_URL)
     except requests.exceptions.HTTPError as error:
         exit('Ошибка:\n{0}'.format(error))
 
     if title:
         upload_url = request_upload_url(access_token, vk_group_id, VK_API_URL)
-        upload_comics = upload_photo_to_server(random_comic_id, upload_url)
+        upload_comics = upload_photo_to_server(random_comics_id, upload_url)
         image_server = upload_comics['server']
         image_hash = upload_comics['hash']
         image_photo = upload_comics['photo']
@@ -119,8 +122,9 @@ def main():
             image_photo)
         media_id = get_save_response['id']
         media_owner_id = get_save_response['owner_id']
-        post_comic(access_token, vk_group_id, VK_API_URL, title, media_id,
-                   media_owner_id)
+        post_comics(access_token, vk_group_id, VK_API_URL, title, media_id,
+                    media_owner_id)
+        delete_comics_file(random_comics_id)
 
 
 if __name__ == "__main__":
